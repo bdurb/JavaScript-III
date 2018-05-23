989b50cebc6f4a026a066908b4e8eef077f53622
Brian <brian@rpmgreenville.com>
2018-05-23 15:02:42 -0700

    Update prototypes.js
    corrected spelling error

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -105,6 +105,6 @@
 */
 
   // Stretch task: 
-  // * Create Villian and Hero classes that inherit from the Humanoid class.  
-  // * Give the Hero and Villians different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
-  // * Create two new objects, one a villian and one a hero and fight it out with methods!
+  // * Create Villain and Hero classes that inherit from the Humanoid class.  
+  // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
+  // * Create two new objects, one a villain and one a hero and fight it out with methods!
